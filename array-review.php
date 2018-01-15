070d2dbe0d3b46b1e7bdb2b719662757a73dbd79
mingjiedeng <mingjie.dmj@gmail.com>
2018-01-15 17:00:44 -0500

    fix the bugs and finished the assignment part 1

--- array-review.php
+++ array-review.php
@@ -1,10 +1,19 @@
 <?php
+/*
+    array-review.php
+    This file is for the Review Assignment: PHP Arrays & Functions
+    in Green River College IT 328 class.
+
+    @author     Mingjie Deng <mdeng@mail.greenriver.edu>
+    @version    1.0
+*/
+
     ini_set('display_errors', 1);
     error_reporting(E_ALL);
 
     $animals = array("panda", "alpaca", "boa");
 
-    function sortAndPrint($myArray) {
+    function sortAndPrint(&$myArray) {
         sort($myArray);
         foreach ($myArray as $value) {
             echo $value . " ";
@@ -12,17 +21,20 @@
         echo "<br>";
     }
 
-    function add($str, $myArray) {
+    function add($str, &$myArray) {
         echo "adding $str...<br>";
-        if (in_array(strtolower($str), array_map("strtolower", $myArray)){
+        if (!in_array(strtolower($str), array_map("strtolower", $myArray))){
             $myArray[] = $str;
-            sortAndPrint($myArray);
         }
+        sortAndPrint($myArray);
     }
 
+    //Output for the assignment part 1
+    echo "--This is the output of the assignment part 1: <br>";
     sortAndPrint($animals);
     add("goat", $animals);
     add("Boa", $animals);
+    echo "<br><br>";
 
 
 
